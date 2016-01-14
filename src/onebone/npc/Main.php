b0a157f85485527a627519fc0a98706381adab4a
onebone <jyc00410@gmail.com>
2016-01-14 23:09:53 +0900

    Initial commit

---
+++ src/onebone/npc/Main.php
@@ -0,0 +1,233 @@
+<?php
+
+/*
+ * Copyright (C) 2015-2016 onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace onebone\npc;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\level\Location;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\network\protocol\InteractPacket;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener{
+	private $npc, $msgQueue;
+
+	public function onEnable(){
+		if(!file_exists($this->getDataFolder())){
+			mkdir($this->getDataFolder());
+		}
+		$this->saveDefaultConfig();
+
+		if(!file_exists($this->getDataFolder()."skins")){
+			mkdir($this->getDataFolder()."skins");
+		}
+		if(!is_file($this->getDataFolder()."npc.dat")){
+			file_put_contents($this->getDataFolder()."npc.dat", serialize([]));
+		}
+		$data = unserialize(file_get_contents($this->getDataFolder()."npc.dat"));
+		$this->npc = [];
+
+		foreach($data as $datam){
+			$skinFile = $this->getDataFolder()."skins/".$datam[6].".skin";
+			$datam[6] = file_get_contents($skinFile);
+			$npc = NPC::createNPC($datam);
+			$this->npc[$npc->getId()] = $npc;
+		}
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onPacketReceived(DataPacketReceiveEvent $event){
+		$pk = $event->getPacket();
+		if($pk instanceof InteractPacket){
+			if(isset($this->npc[$pk->target])){
+				if(isset($this->msgQueue[$event->getPlayer()->getName()])){
+					$npc = $this->npc[$pk->target];
+					$npc->setMessage($this->msgQueue[$event->getPlayer()->getName()]);
+					unset($this->msgQueue[$event->getPlayer()->getName()]);
+					$event->getPlayer()->sendMessage("You have set NPC ".TextFormat::AQUA.$npc->getName().TextFormat::WHITE." to say ".TextFormat::GREEN.$npc->getMessage());
+					return;
+				}else{
+					$this->npc[$pk->target]->onInteract($event->getPlayer());
+				}
+			}
+		}
+	}
+
+	public function onPlayerJoin(PlayerJoinEvent $event){
+		foreach($this->npc as $npc){
+			if($npc->getLevel()->getFolderName() === $event->getPlayer()->getLevel()->getFolderName()){
+				$npc->spawnTo($event->getPlayer());
+			}
+		}
+	}
+
+	public function onMoveEvent(PlayerMoveEvent $event){
+		$player = $event->getPlayer();
+
+		if($event->getFrom()->getLevel()->getFolderName() !== ($toLevel = $event->getTo()->getLevel()->getFolderName())){
+			foreach($this->npc as $npc){
+				if($npc->getLevel()->getFolderName() === $toLevel){
+					$npc->spawnTo($player);
+				}else{
+					$npc->removeFrom($player);
+				}
+			}
+		}
+		foreach($this->npc as $npc){
+			if($npc->getLevel()->getFolderName() === $event->getPlayer()->getLevel()->getFolderName()){
+				$npc->seePlayer($player);
+			}
+		}
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, $label, array $params){
+		switch($command->getName()){
+			case "npc":
+			switch(strtolower(array_shift($params))){
+				case "create":
+				case "c":
+				if(!$sender->hasPermission("npc.command.npc.create")){
+					$sender->sendMessage(TextFormat::RED."You don't have permission to use this command.");
+					return true;
+				}
+
+				$name = implode(" ", $params);
+				if(trim($name) === ""){
+					$sender->sendMessage(TextFormat::RED."Usage: /npc create <name>");
+					return true;
+				}
+				$location = new Location($sender->getX(), $sender->getY(), $sender->getZ(), -1, -1, $sender->getLevel());
+
+				$npc = new NPC(clone $location, $name, $sender->getSkinData(), $sender->getSkinName(), $sender->getInventory()->getItemInHand());
+				$this->npc[$npc->getId()] = $npc;
+				foreach($sender->getLevel()->getPlayers() as $player){
+					$npc->spawnTo($player);
+				}
+
+				if($this->getConfig()->get("save-on-change")){
+					$this->save();
+				}
+				return true;
+				case "remove":
+				case "r":
+				if(!$sender->hasPermission("npc.command.npc.remove")){
+					$sender->sendMessage(TextFormat::RED."You don't have permission to use this command.");
+					return true;
+				}
+
+				$id = array_shift($params);
+				if(!is_numeric($id)){
+					$sender->sendMessage(TextFormat::RED."Usage: /npc remove <id>");
+					return true;
+				}
+
+				foreach($this->npc as $key => $npc){
+					if($id == $npc->getId()){
+						$npc->remove();
+						unset($this->npc[$key]);
+						$sender->sendMessage("Removed NPC ".TextFormat::AQUA.$npc->getName());
+						if($this->getConfig()->get("save-on-change")){
+							$this->save();
+						}
+						return true;
+					}
+				}
+				$sender->sendMessage("Could not find NPC ".TextFormat::RED.$id);
+				return true;
+				case "list":
+				case "ls":
+				case "l":
+				if(!$sender->hasPermission("npc.command.npc.list")){
+					$sender->sendMessage(TextFormat::RED."You don't have permission to use this command.");
+					return true;
+				}
+
+				$page = array_shift($params);
+				if(!is_numeric($page)){
+					$page = 1;
+				}
+
+				$max = ceil(count($this->npc)/5);
+				$page = (int)$page;
+				$page = max(1, min($page, $max));
+
+				$output = "Showing NPC list (page $page/$max): \n";
+				$n = 0;
+				foreach($this->npc as $id => $npc){
+					$current = (int)ceil(++$n / 5);
+
+					if($current === $page){
+						$output .= "#".$npc->getId()
+						." (".round($npc->pos->x, 2).":".round($npc->pos->y, 2).":".round($npc->pos->z, 2).":".$npc->pos->getLevel()->getName()."): "
+						.$npc->getName()."\n";
+					}elseif($current > $page) break;
+				}
+				$output = substr($output, 0, -1);
+				$sender->sendMessage($output);
+				return true;
+				case "message":
+				case "msg":
+				case "m":
+				if(!$sender->hasPermission("npc.command.npc.message")){
+					$sender->sendMessage(TextFormat::RED."You don't have permission to use this command.");
+					return true;
+				}
+
+				$message = trim(implode(" ", $params));
+
+				$this->msgQueue[$sender->getName()] = $message;
+
+				$sender->sendMessage("Touch NPC you want to set message.");
+				if($this->getConfig()->get("save-on-change")){
+					$this->save();
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function onDisable(){
+		$this->save();
+	}
+
+	public function save(){
+		$dir = scandir($this->getDataFolder()."skins/");
+		foreach($dir as $file){
+			if($file !== "." and $file !== ".."){
+				unlink($this->getDataFolder()."skins/".$file);
+			}
+		}
+
+		$save = [];
+		foreach($this->npc as $npc){
+			$data = $npc->getSaveData();
+			file_put_contents($this->getDataFolder()."skins/".$data[6].".skin", $npc->getSkin());
+			$save[] = $data;
+		}
+		file_put_contents($this->getDataFolder()."npc.dat", serialize($save));
+	}
+}
---
+++ src/onebone/npc/NPC.php
@@ -0,0 +1,203 @@
+<?php
+
+/*
+ * Copyright (C) 2015-2016 onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace onebone\npc;
+
+use pocketmine\entity\Human;
+use pocketmine\entity\Entity;
+use pocketmine\level\Location;
+use pocketmine\network\Network;
+use pocketmine\Server;
+use pocketmine\network\protocol\AddPlayerPacket;
+use pocketmine\network\protocol\MovePlayerPacket;
+use pocketmine\network\protocol\RemovePlayerPacket;
+use pocketmine\network\protocol\RemoveEntityPacket;
+use pocketmine\utils\UUID;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\math\Vector2;
+
+class NPC{
+	private $eid;
+	public $pos;
+	private $skin, $skinName, $name;
+	private $item, $message;
+
+	private $uuid;
+
+	public function __construct(Location $loc, $name, $skin, $skinName, Item $item, $message = ""){
+		$this->pos = $loc;
+		$this->eid = Entity::$entityCount++;
+		$this->skin = $skin;
+		$this->skinName = $skinName;
+		$this->name = $name;
+		$this->item = $item;
+		$this->message = $message;
+
+		$this->uuid = UUID::fromRandom();
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setMessage($msg){
+		$this->message = $msg;
+	}
+
+	public function getMessage(){
+		return $this->message;
+	}
+
+	public function setX($x){
+		$this->pos->x = $x;
+	}
+
+	public function setY($y){
+		$this->pos->y = $y;
+	}
+
+	public function setZ($z){
+		$this->pos->z = $z;
+	}
+
+	public function setYaw($yaw){
+		$this->pos->yaw = $yaw;
+	}
+
+	public function setPitch($pitch){
+		$this->pos->pitch = $pitch;
+	}
+
+	public function getLevel(){
+		return $this->pos->level;
+	}
+
+	public function getSkin(){
+		return $this->skin;
+	}
+
+	public function setHoldingItem(Item $item){
+		$this->item = $item->getId();
+		$this->meta = $item->getDamage();
+	}
+
+	public function getId(){
+		return $this->eid;
+	}
+
+	public function onInteract(Player $player){
+		if($this->message !== ""){
+			$player->sendMessage($this->message);
+		}
+	}
+
+	public function seePlayer(Player $target){
+		$pk = new MovePlayerPacket();
+		$pk->eid = $this->eid;
+		if($this->pos->yaw === -1 and $target !== null){
+			$xdiff = $target->x - $this->pos->x;
+			$zdiff = $target->z - $this->pos->z;
+			$angle = atan2($zdiff, $xdiff);
+			$pk->yaw = (($angle * 180) / M_PI) - 90;
+		}else{
+			$pk->yaw = $this->pos->yaw;
+		}
+		if($this->pos->pitch === -1 and $target !== null){
+			$ydiff = $target->y - $this->pos->y;
+
+			$vec = new Vector2($this->pos->x, $this->pos->z);
+			$dist = $vec->distance($target->x, $target->z);
+			$angle = atan2($dist, $ydiff);
+			$pk->pitch = (($angle * 180) / M_PI) - 90;
+		}else{
+			$pk->pitch = $this->pitch;
+		}
+		$pk->x = $this->pos->x;
+		$pk->y = $this->pos->y + 1.62;
+		$pk->z = $this->pos->z;
+		$pk->bodyYaw = $pk->yaw;
+		$pk->onGruond = 0;
+
+		$target->dataPacket($pk);
+	}
+
+	public function spawnTo(Player $target){
+		$pk = new AddPlayerPacket();
+		$pk->uuid = $this->uuid;
+		$pk->username = $this->name;
+		$pk->eid = $this->eid;
+		$pk->x = $this->pos->x;
+		$pk->y = $this->pos->y;
+		$pk->z = $this->pos->z;
+		if($this->pos->yaw === -1 and $target !== null){
+			$xdiff = $target->x - $this->pos->x;
+			$zdiff = $target->z - $this->pos->z;
+			$angle = atan2($zdiff, $xdiff);
+			$pk->yaw = (($angle * 180) / M_PI) - 90;
+		}else{
+			$pk->yaw = $this->pos->yaw;
+		}
+		if($this->pos->pitch === -1 and $target !== null){
+
+		}else{
+			$pk->pitch = $this->pos->pitch;
+		}
+		$pk->item = $this->item;
+		$pk->skin = $this->skin;
+		$pk->metadata =
+		[
+			Entity::DATA_SHOW_NAMETAG => [
+						Entity::DATA_TYPE_BYTE,
+						1
+				],
+		];
+		$target->dataPacket($pk);
+	}
+
+	public function removeFrom(Player $player){
+		$pk = new RemovePlayerPacket();
+		$pk->clientId = $this->uuid;
+		$pk->eid = $this->eid;
+
+		$player->directDataPacket($pk);
+	}
+
+	public function remove(){
+		$pk = new RemovePlayerPacket();
+		$pk->eid = $this->eid;
+		$pk->clientId = $this->uuid;
+		$players = $this->pos->level->getPlayers();
+		foreach($players as $player){
+			$player->directDataPacket($pk);
+		}
+	}
+
+	public function getSaveData(){
+		return [
+			$this->pos->x, $this->pos->y, $this->pos->z, $this->pos->level->getFolderName(),
+			$this->pos->yaw, $this->pos->pitch,
+			$this->eid, $this->item->getId(), $this->item->getDamage(), $this->name, $this->skinName, $this->message
+		];
+	}
+
+	public static function createNPC($data){
+		return new NPC(new Location($data[0], $data[1], $data[2], $data[4], $data[5], Server::getInstance()->getLevelByName($data[3])), $data[9], $data[6], $data[10], Item::get($data[7], $data[8]), $data[11]);
+	}
+}
