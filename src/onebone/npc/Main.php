149e7652db91389a57c92fbfbeb6d96d496c32bd
NLOG <sorisem4106@naver.com>
2017-08-09 11:54:06 +0900

    Support PM 3.0.0-ALPHA7 : MCPE 1.0.0 (#23)
    * Support PocketMine-MP 3.0.0-ALPHA7
    
    * api bump
    
    * mistake
    
    * ...
    
    * PM's command API was updated
    
    * Update Main.php

--- src/onebone/npc/Main.php
+++ src/onebone/npc/Main.php
@@ -30,7 +30,7 @@ use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\entity\EntityTeleportEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\network\protocol\InteractPacket;
+use pocketmine\network\mcpe\protocol\InteractPacket;
 use pocketmine\Player;
 
 class Main extends PluginBase implements Listener{
@@ -124,7 +124,7 @@ class Main extends PluginBase implements Listener{
 		}
 	}
 
-	public function onCommand(CommandSender $sender, Command $command, $label, array $params){
+	public function onCommand(CommandSender $sender, Command $command, $label, array $params) : bool {
 		switch($command->getName()){
 			case "npc":
 			switch(strtolower(array_shift($params))){
